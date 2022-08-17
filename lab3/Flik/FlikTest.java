f365a63158a3cd9264bfc94622a7359e15bb12c1
colorful <3235942504@qq.com>
2022-08-17 17:27:07 +0800

    22/8/17

---
+++ lab3/Flik/FlikTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class FlikTest {
+    @Test
+    public void testisSameNumber(){
+        int a = 128;
+        int b = 128;
+        boolean l = Flik.isSameNumber(a, b);
+        assertTrue("FFF",l);
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -69,5 +69,14 @@ public class IntListTest {
     /** If you're running this from the command line, you'll need
       * to add a main method. See ArithmeticTest.java for an
       * example. */
+    @Test(timeout = 1000)
+    public void testReverse(){
+        IntList A = IntList.of(1,2,3,4,5);
+        IntList B = IntList.reverse(A);
+        assertEquals(IntList.of(5,4,3,2,1),B);
+        assertEquals(new IntList(),IntList.reverse(new IntList()));
+        assertNotEquals(A,B);
+
+    }
 
 }
